8f65c688745c5cbe5b0de56d7a97a85c9f747048
jsjshub <iamjsjs0319@gmail.com>
2021-05-18 15:36:24 +0900

    visualization

--- jsjs/visualization/type.js
+++ jsjs/visualization/type.js
@@ -1,6 +1,6 @@
-function line_bar_chart(canvas, label, dataset, type) {
+function line_bar_chart(canvas, labels, dataset, type) {
     const data = {
-        labels: label,
+        labels: labels,
         datasets: dataset
     };
     const config = {
@@ -23,6 +23,30 @@ function circle_chart(canvas, labels, dataset, type) {
     const config = {
         type: type,
         data: data,
+    };
+
+    const myChart = new Chart(
+        canvas,
+        config
+    );
+}
+
+function mixed_chart(canvas, labels, dataset ){
+    const data = {
+        labels: labels,
+        datasets: dataset,
+    };
+
+    const config = {
+        type: 'scatter',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
     };
 
     const myChart = new Chart(
